7d2d646ee643ccd4091c5b161bec790417387ac6
alourenco <alourenco@DCCNOTE286>
2023-02-17 17:25:26 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio01Balta/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Desafio01Balta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*************DESAFIO 01 - BALTA.IO**************");
+            Console.WriteLine("");
+            Console.WriteLine("Digite um texto:");
+            var texto = Console.ReadLine();
+            var caracteres = ContarCaracteresTexto(texto);
+            var palavras = ContarPalavrasTexto(texto);
+            Console.WriteLine(String.Format("{0} caracteres, {1} palavras", caracteres, palavras));
+            Console.ReadKey();
+        }
+
+        private static int ContarPalavrasTexto(string texto)
+        {
+            var textoSeparado = texto.Split(' ');
+            return textoSeparado.Length;
+        }
+
+        private static int ContarCaracteresTexto(string texto)
+        {
+            int soma = 0;
+            var textoSeparado = texto.Split(' ');
+            foreach (var item in textoSeparado)
+            {
+                soma += item.Length;
+            }
+            return soma;
+        }
+    }
+}
